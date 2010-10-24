ae1b193f5880a127d87471b339520a44c1edbc5a
Ryan Roemer <ryan@loose-bits.com>
2010-10-23 20:11:31 -0700

    Add fabfile, basic run targets.

--- cloud_browser_project/settings.py
+++ cloud_browser_project/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'cloud_browser.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
