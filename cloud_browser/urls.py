f14d0b8eaa7bfc0795046289e3d3b74ec9c2e862
Ryan Roemer <ryan@loose-bits.com>
2010-10-28 08:36:17 -0700

    Views: Split path into folder, file parts.

--- cloud_browser/urls.py
+++ cloud_browser/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('cloud_browser.views',
-    url(r'^$', redirect_to, name="cloud_browser_index", kwargs={'url': 'browser'}),
+    url(r'^$', redirect_to, name="cloud_browser_index",
+        kwargs={'url': 'browser'}),
     url(r'browser/(?P<path>.*)$', 'browser', name="cloud_browser_browser"),
 )
--- cloud_browser/views.py
+++ cloud_browser/views.py
@@ -4,8 +4,19 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
+def _path_parts(path=''):
+    """Split path into folder, file tuple."""
+    path = path if path is not None else ''
+    parts = path.strip('/').split('/')
+    if len(parts) == 1:
+        return parts[0], ''
+
+    return parts[0], '/'.join(parts[1:])
+    
+
 def browser(request, path='', template="cloud_browser/browser.html"):
     """Basic browser view."""
+    folder, file = _path_parts(path)
     return render_to_response(template,
-                              {'path': path},
+                              {'folder': folder, 'file': file},
                               context_instance=RequestContext(request))
