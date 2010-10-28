35cae39dc77e0e4421c6d661e157c73d289f38b4
Ryan Roemer <ryan@loose-bits.com>
2010-10-28 08:27:19 -0700

    Urls/Templates: Add path variable.

--- cloud_browser/urls.py
+++ cloud_browser/urls.py
@@ -3,5 +3,5 @@ from django.views.generic.simple import redirect_to
 
 urlpatterns = patterns('cloud_browser.views',
     url(r'^$', redirect_to, name="cloud_browser_index", kwargs={'url': 'browser'}),
-    url(r'browser/$', 'browser', name="cloud_browser_browser"),
+    url(r'browser/(?P<path>.*)$', 'browser', name="cloud_browser_browser"),
 )
--- cloud_browser/views.py
+++ cloud_browser/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
-def browser(request, template="cloud_browser/browser.html"):
+def browser(request, path='', template="cloud_browser/browser.html"):
     """Basic browser view."""
     return render_to_response(template,
+                              {'path': path},
                               context_instance=RequestContext(request))
