b1b23ad9b3908f94a32b45f7033bc84ce1a2b056
Ryan Roemer <ryan@loose-bits.com>
2010-10-29 08:32:56 -0700

    Browser: Can render pseudo-dirs, but need the real marker.

--- cloud_browser/views.py
+++ cloud_browser/views.py
@@ -1,4 +1,9 @@
 """Cloud browser views."""
+# TODO: Need to add directory "application/directory" markers everywhere.
+# Probably should consider writing a stupid crawler.
+
+import os.path
+
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render_to_response
 from django.template import RequestContext
@@ -12,30 +17,35 @@ def _path_parts(path=''):
     if len(parts) == 1:
         return parts[0], ''
 
-    return parts[0], '/'.join(parts[1:])
-    
+    return parts[0], _path_join(*parts[1:])
+
+def _path_join(*args):
+    """Custom path joining."""
+    return '/'.join((x for x in args if x not in (None, '')))
 
 def browser(request, path='', template="cloud_browser/browser.html"):
     """Basic browser view."""
-    folder, file = _path_parts(path)
+    folder_path, file_path = _path_parts(path)
     folder_infos = None
     file_infos = None
     file_limit = 20
     conn = get_connection()
 
-    if folder == '':
+    if folder_path == '':
         # List folders.
         folder_infos = conn.list_containers_info()
 
     else:
         # List files for a folder.
-        folder_obj = conn.get_container(folder)
+        folder_obj = conn.get_container(folder_path)
         file_infos = folder_obj.list_objects_info(
-            prefix=file, limit=file_limit)
+            path=file_path, limit=file_limit)
         for info in file_infos:
-            info['path'] = '/'.join((folder, info['name']))
+            info['path'] = _path_join(folder_path, info['name'])
+            info['rel_path'] = info['name'].lstrip(file_path).lstrip('/')
 
     return render_to_response(template,
-                              {'folder': folder, 'folder_infos': folder_infos,
-                               'file': file, 'file_infos': file_infos},
+                              {'folder': folder_path,
+                               'folder_infos': folder_infos,
+                               'file': file_path, 'file_infos': file_infos},
                               context_instance=RequestContext(request))
