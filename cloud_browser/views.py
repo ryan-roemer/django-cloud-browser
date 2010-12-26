7ab22a685a4b41a536a89485b3a5f8078b61a50f
Ryan Roemer <ryan@loose-bits.com>
2010-12-25 22:07:09 -0500

    Refactor.

--- cloud_browser/views.py
+++ cloud_browser/views.py
@@ -25,6 +25,46 @@ def _path_join(*args):
     return '/'.join((x for x in args if x not in (None, '')))
 
 
+def _get_files(folder_obj, folder_path, file_path):
+    """Get files."""
+    # List Objects Info Issue:
+    #
+    # If there are not psuedo-directories, then a `path` parameter to
+    # `list_objects_info` will not match any contained folders. On the
+    # Other hand, `prefix` parameter matching will always work, but will
+    # be overly constrained.
+    #
+    # We employ the following heuristic. If there is a pseudo-directory
+    # at the current level, we **do** use prefix, *else* we use path.
+
+    file_limit = 20
+    list_kwargs = {
+        'limit': file_limit,
+    }
+
+    use_path = False
+    if file_path != '':
+        try:
+            file_obj = folder_obj.get_object(file_path)
+            use_path = file_obj.content_type == 'application/directory'
+        except cloudfiles.errors.NoSuchObject:
+            pass
+
+    if use_path or file_path == '':
+        list_kwargs['path'] = file_path
+    else:
+        list_kwargs['prefix'] = file_path
+
+    # List files for a folder.
+    file_infos = folder_obj.list_objects_info(  # pylint: disable=W0142
+        **list_kwargs)
+    for info in file_infos:
+        info['path'] = _path_join(folder_path, info['name'])
+        info['rel_path'] = info['name'].lstrip(file_path).lstrip('/')
+
+    return file_infos
+
+
 def browser(request, path='', template="cloud_browser/browser.html"):
     """View files in a file path.
 
@@ -42,7 +82,6 @@ def browser(request, path='', template="cloud_browser/browser.html"):
     folder_path, file_path = _path_parts(path)
     folder_infos = None
     file_infos = None
-    file_limit = 20
     conn = get_connection()
 
     if folder_path == '':
@@ -51,43 +90,12 @@ def browser(request, path='', template="cloud_browser/browser.html"):
 
     else:
         folder_obj = conn.get_container(folder_path)
-
-        # List Objects Info Issue:
-        #
-        # If there are not psuedo-directories, then a `path` parameter to
-        # `list_objects_info` will not match any contained folders. On the
-        # Other hand, `prefix` parameter matching will always work, but will
-        # be overly constrained.
-        #
-        # We employ the following heuristic. If there is a pseudo-directory
-        # at the current level, we **do** use prefix, *else* we use path.
-
-        list_kwargs = {
-            'limit': file_limit,
-        }
-
-        use_path = False
-        if file_path != '':
-            try:
-                file_obj = folder_obj.get_object(file_path)
-                use_path = file_obj.content_type == 'application/directory'
-            except cloudfiles.errors.NoSuchObject:
-                pass
-
-        if use_path:
-            list_kwargs['path'] = file_path
-        else:
-            list_kwargs['prefix'] = file_path
-
-        # List files for a folder.
-        file_infos = folder_obj.list_objects_info(  # pylint: disable=W0142
-            **list_kwargs)
-        for info in file_infos:
-            info['path'] = _path_join(folder_path, info['name'])
-            info['rel_path'] = info['name'].lstrip(file_path).lstrip('/')
+        file_infos = _get_files(folder_obj, folder_path, file_path)
 
     return render_to_response(template,
                               {'folder': folder_path,
                                'folder_infos': folder_infos,
-                               'file': file_path, 'file_infos': file_infos},
+                               'path': path,
+                               'file': file_path,
+                               'file_infos': file_infos},
                               context_instance=RequestContext(request))
