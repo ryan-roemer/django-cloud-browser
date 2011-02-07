a35234cc7fa846d123dc6ae85e35a0ba8852e6c4
Ryan Roemer <ryan@loose-bits.com>
2011-02-07 08:41:24 -0800

    Decompose template tag better.

--- cloud_browser/templatetags/cloud_browser_extras.py
+++ cloud_browser/templatetags/cloud_browser_extras.py
@@ -18,6 +18,15 @@ def truncatechars(value, num, end_text="..."):
     .. note::
         Django ticket `5025 <http://code.djangoproject.com/ticket/5025>`_ has a
         patch for a more extensible and robust truncate characters tag filter.
+
+    Example::
+
+        {{ my_variable|truncatechars:22 }}
+
+    :param value: Value to truncate.
+    :type  value: ``string``
+    :param num: Number of characters to trim to.
+    :type  num: ``int``
     """
     length = None
     try:
@@ -34,11 +43,26 @@ truncatechars.is_safe = True  # pylint: disable=W0612
 
 @register.tag
 def cloud_browser_media_link(_, token):
-    """Create appropriate ``link``, ``script`` or ``style`` tag from relative
-    resource path.
+    """Create appropriate ``link``, ``script`` or ``style`` tag for JavaScript
+    and CSS files from relative resource path.
+
+    .. note::
+        Presently only works for JavaScript and CSS because those *can* be
+        dumped inline into an HTML page. Unfortunately, images cannot, and we
+        haven't decided how to handle that yet (maybe ignore if not statically
+        served?)
 
     Correctly handles whether or not the settings variable
     ``CLOUD_BROWSER_STATIC_MEDIA_DIR`` is set and served.
+
+    For example, if we use the tag in a template::
+
+        {% cloud_browser_media_link "js/browser.js" %}
+
+    And the static media directory variable is set and media is served, then
+    a ``<script>`` tag **with** a ``link`` is emitted, pointing to the URL.
+    However, if the directory variable is not set, a ``<script>`` tag with
+    the full inline JavaScript text is emitted instead.
 
     """
     bits = token.split_contents()
@@ -62,6 +86,12 @@ class MediaLinkNode(Node):
         ],
     }
 
+    #: The static served media URL (or ``None``)
+    static_media_url = settings.app_media_url
+
+    #: The fallback template path of the same media.
+    template_media_path = 'cloud_browser_media'
+
     def __init__(self, rel_path):
         """Initializer."""
         super(MediaLinkNode, self).__init__()
@@ -73,14 +103,15 @@ class MediaLinkNode(Node):
         from django.template.loader_tags import IncludeNode
 
         # Check if we have static-served media
-        if settings.app_media_url:
+        if self.static_media_url:
             # Use a link.
-            path = os.path.join(settings.app_media_url, self.rel_path)
-            return self.mappings[self.media_type][0] % path
+            static_path = os.path.join(self.static_media_url, self.rel_path)
+            return self.mappings[self.media_type][0] % static_path
 
         else:
             # Have to render page with full media included.
-            template_path = os.path.join('cloud_browser_media', self.rel_path)
+            template_path = os.path.join(self.template_media_path,
+                                         self.rel_path)
             template_path = '"' + template_path.strip('"') + '"'
             rendered = IncludeNode(template_path).render(context)
             return self.mappings[self.media_type][1] % rendered
