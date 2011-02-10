7182f52f495174dc7a9689100f5298e848b8229c
Ryan Roemer <ryan@loose-bits.com>
2011-02-09 19:15:22 -0800

    Setup: Add long description read straight from README.

--- setup.py
+++ setup.py
@@ -1,18 +1,42 @@
 """Cloud browser package."""
-
+from __future__ import with_statement
+import os
 from setuptools import setup, find_packages
 
 from cloud_browser import __version__
 
+
+###############################################################################
 # Base packages.
+###############################################################################
 MOD_NAME = "cloud_browser"
 PKGS = [x for x in find_packages() if x.split('.')[0] == MOD_NAME]
 
+
+###############################################################################
+# Helpers.
+###############################################################################
+def read_file(name):
+    """Read file name (without extension) to string."""
+    cur_path = os.path.dirname(__file__)
+    exts = ('txt', 'rst')
+    for ext in exts:
+        path = os.path.join(cur_path, '.'.join((name, ext)))
+        if os.path.exists(path):
+            with open(path, 'rb') as file_obj:
+                return file_obj.read()
+
+    return ''
+
+
+###############################################################################
+# Setup.
+###############################################################################
 setup(
     name="django-cloud-browser",
     version=__version__,
     description="Django Cloud Browser application.",
-    long_description="Browser for cloud datastores (Rackspace, AWS, etc.).",
+    long_description=read_file("README"),
     url="https://github.com/ryan-roemer/django-cloud-browser",
 
     author="Ryan Roemer",
