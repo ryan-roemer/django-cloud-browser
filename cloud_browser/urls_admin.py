d3f242dcbd2fdcbc0531c72e4ad4dfef5b7a7f43
Ryan Roemer <ryan@loose-bits.com>
2011-02-25 10:57:23 -0500

    pep8

--- cloud_browser/urls_admin.py
+++ cloud_browser/urls_admin.py
@@ -5,7 +5,7 @@ from django.views.generic.simple import redirect_to
 urlpatterns = patterns('cloud_browser.views',  # pylint: disable=C0103
     url(r'^$', redirect_to, name="cloud_browser_index",
         kwargs={'url': 'browser'}),
-    url(r'^browser/(?P<path>.*)$', 'browser',  name="cloud_browser_browser",
+    url(r'^browser/(?P<path>.*)$', 'browser', name="cloud_browser_browser",
         kwargs={'template': "cloud_browser/admin/browser.html"}),
     url(r'^document/(?P<path>.*)$', 'document', name="cloud_browser_document"),
 )
