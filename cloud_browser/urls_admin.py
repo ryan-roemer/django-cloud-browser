37940024589e55d865d82b6b51dae7b8b762ed32
Ryan Roemer <ryan@loose-bits.com>
2011-02-25 10:45:46 -0500

    Admin: Add admin URLs and templates integration. Provide example in project.

---
+++ cloud_browser/urls_admin.py
@@ -0,0 +1,11 @@
+"""Cloud browser URLs for Django admin integration."""
+from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import redirect_to
+
+urlpatterns = patterns('cloud_browser.views',  # pylint: disable=C0103
+    url(r'^$', redirect_to, name="cloud_browser_index",
+        kwargs={'url': 'browser'}),
+    url(r'^browser/(?P<path>.*)$', 'browser',  name="cloud_browser_browser",
+        kwargs={'template': "cloud_browser/admin/browser.html"}),
+    url(r'^document/(?P<path>.*)$', 'document', name="cloud_browser_document"),
+)
--- cloud_browser_project/urls.py
+++ cloud_browser_project/urls.py
@@ -9,8 +9,13 @@ from django.views.generic.simple import redirect_to
 admin.autodiscover()
 
 urlpatterns = patterns('',  # pylint: disable=C0103
-    url(r'^$', redirect_to, name='index', kwargs={'url': 'cb/'}),
-    url(r'^cb/', include('cloud_browser.urls')),
+    # Normal URLs.
+    #url(r'^$', redirect_to, name='index', kwargs={'url': 'cb/'}),
+    #url(r'^cb/', include('cloud_browser.urls')),
+
+    # Admin URLs. Note: Include ``urls_admin`` **before** admin.
+    url(r'^$', redirect_to, name='index', kwargs={'url': 'admin/'}),
+    url(r'^admin/cb/', include('cloud_browser.urls_admin')),
 
     # Hack in the bare minimum to get accounts support.
     url(r'^admin/', include(admin.site.urls)),
