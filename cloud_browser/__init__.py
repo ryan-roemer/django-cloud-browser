3730db1c9ac628c9702fed5de4c64313f8c7bf90
Ryan Roemer <ryan@loose-bits.com>
2011-03-15 23:12:24 -0700

    App Media: Add admin URLs static server. Also bump version number for next 0.1.3 dev.

--- cloud_browser/__init__.py
+++ cloud_browser/__init__.py
@@ -3,7 +3,7 @@
 Provides a simple filesystem-like browser interface for cloud blob datastores.
 """
 
-VERSION = (0, 1, 2, "alpha", 0)
+VERSION = (0, 1, 3, "alpha", 0)
 
 __version__ = ".".join(str(v) for v in VERSION[:3])
 __version_full__ = __version__ + "".join(str(v) for v in VERSION[3:])
--- cloud_browser/urls_admin.py
+++ cloud_browser/urls_admin.py
@@ -2,6 +2,8 @@
 from django.conf.urls.defaults import patterns, url
 from django.views.generic.simple import redirect_to
 
+from cloud_browser.app_settings import settings
+
 urlpatterns = patterns('cloud_browser.views',  # pylint: disable=C0103
     url(r'^$', redirect_to, name="cloud_browser_index",
         kwargs={'url': 'browser'}),
@@ -9,3 +11,11 @@ urlpatterns = patterns('cloud_browser.views',  # pylint: disable=C0103
         kwargs={'template': "cloud_browser/admin/browser.html"}),
     url(r'^document/(?P<path>.*)$', 'document', name="cloud_browser_document"),
 )
+
+if settings.app_media_url is None:
+    # Use a static serve.
+    urlpatterns += patterns('',  # pylint: disable=C0103
+        url(r'^app_media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.app_media_doc_root},
+            name="cloud_browser_media"),
+    )
