1cbb345f7c539239aaf30ee06a8df7b7e4a5f693
Ryan Roemer <ryan@loose-bits.com>
2011-04-30 17:31:51 -0400

    Version bump to 0.2.1 and mark beta.  Will have to revise a vs. b vs. f later.

--- cloud_browser/__init__.py
+++ cloud_browser/__init__.py
@@ -3,7 +3,7 @@
 Provides a simple filesystem-like browser interface for cloud blob datastores.
 """
 
-VERSION = (0, 2, 0, "alpha", 1)
+VERSION = (0, 2, 1, "beta", 0)
 
 __version__ = ".".join(str(v) for v in VERSION[:3])
 __version_full__ = __version__ + "".join(str(v) for v in VERSION[3:])
