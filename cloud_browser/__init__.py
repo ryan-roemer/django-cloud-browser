d9844f5bcf6d48bde1a60d32998ccdaa87e99676
Ryan Roemer <ryan@loose-bits.com>
2011-12-14 10:11:04 -0500

    Version: Fix __version_full__ for new scheme.

--- cloud_browser/__init__.py
+++ cloud_browser/__init__.py
@@ -6,4 +6,4 @@ Provides a simple filesystem-like browser interface for cloud blob datastores.
 VERSION = (0, 2, 1)
 
 __version__ = ".".join(str(v) for v in VERSION)
-__version_full__ = __version__ + "".join(str(v) for v in VERSION)
+__version_full__ = __version__
