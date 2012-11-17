7d3bf7779cdfe8fe225d82605da6abf975dfe7f4
Ryan Roemer <ryan@loose-bits.com>
2012-11-17 06:37:04 -0500

    Docs changes

--- doc/conf.py
+++ doc/conf.py
@@ -105,7 +105,9 @@ html_short_title = "Cloud Browser"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+  'source_link_position': "footer",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
