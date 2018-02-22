4d4b412bf67b782fc35d12531a9263c38230a96b
Jeff Corcoran <jcorcoran+github@gmail.com>
2018-02-22 00:00:55 -0500

    Update URL patterns for Django >= 1.8

--- cloud_browser_project/urls.py
+++ cloud_browser_project/urls.py
@@ -12,35 +12,31 @@ ADMIN_URLS = False
 urlpatterns = patterns('')  # pylint: disable=C0103
 
 if ADMIN_URLS:
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         # Admin URLs. Note: Include ``urls_admin`` **before** admin.
         url(r'^$', RedirectView.as_view(url='/admin/'), name='index'),
         url(r'^admin/cb/', include('cloud_browser.urls_admin')),
-    )
+    ]
 
 else:
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         # Normal URLs.
         url(r'^$', RedirectView.as_view(url='/cb/'), name='index'),
         url(r'^cb/', include('cloud_browser.urls')),
-    )
+    ]
 
-urlpatterns += patterns(
-    '',
+urlpatterns += [
     # Hack in the bare minimum to get accounts support.
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url(r'^accounts/$', RedirectView.as_view(url='/login/')),
     url(r'^accounts/profile', RedirectView.as_view(url='/')),
-)
+]
 
 if settings.DEBUG:
     # Serve up static media.
-    urlpatterns += patterns(
-        '',
+    urlpatterns += [
         url(r'^' + settings.MEDIA_URL.strip('/') + '/(?P<path>.*)$',
             'django.views.static.serve',
             {'document_root': settings.MEDIA_ROOT}),
-    )
+    ]
