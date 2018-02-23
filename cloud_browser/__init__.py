b06cdd24885a6309e843ed924dbf1705b67e7f48
Ryan Roemer <ryan@loose-bits.com>
2018-02-22 16:01:58 -0800

    Version 0.4.0

--- cloud_browser/__init__.py
+++ cloud_browser/__init__.py
@@ -3,7 +3,7 @@
 Provides a simple filesystem-like browser interface for cloud blob datastores.
 """
 
-VERSION = (0, 3, 0)
+VERSION = (0, 4, 0)
 
 __version__ = ".".join(str(v) for v in VERSION)
 __version_full__ = __version__
